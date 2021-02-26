07b744f59e688f2b173d8204687d402d7bbc0a76
Erik Morén <erik.moren@afconsult.com>
2021-02-26 11:23:35 +0100

    dialog

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from "./Modal";
+import './app.css';
 
 function App()
 {
+  const [open, setOpen] = useState(false);
+  
   return (
-    <div>Hello</div>
+    <div>
+      <div>Hello</div>
+      <button onClick={(e) => setOpen(!open)}>Click to toggle</button>
+      <Modal open={open} onRequestClose={() => setOpen(false)} closeOnOutsideClick>
+        I dialogen
+      </Modal>
+    </div>
   );
 }
 
